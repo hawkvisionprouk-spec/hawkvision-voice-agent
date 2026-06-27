910a5fa4df45e7f10f6412daa0bd20526c8a29cf
hawkvisionprouk-spec <hawkvisionpro.uk@gmail.com>
2026-06-28 00:56:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,6 @@ wss.on('connection', (twilioWs) => {
       {
         headers: {
           Authorization: `Bearer ${OPENAI_API_KEY}`,
-          'OpenAI-Beta': 'realtime=v1',
         },
       }
     );
