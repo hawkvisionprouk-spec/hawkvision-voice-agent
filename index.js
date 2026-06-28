45afdf2aeffcb40cd8081fb9851e6798a343deb7
hawkvisionprouk-spec <hawkvisionpro.uk@gmail.com>
2026-06-28 11:09:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -61,12 +61,6 @@ wss.on('connection', (twilioWs) => {
       openAiWs.send(JSON.stringify({
         type: 'session.update',
         session: {
-          turn_detection: {
-            type: 'server_vad',
-            threshold: 0.5,
-            prefix_padding_ms: 300,
-            silence_duration_ms: 500
-          },
           input_audio_format: 'g711_ulaw',
           output_audio_format: 'g711_ulaw',
           voice: 'alloy',
