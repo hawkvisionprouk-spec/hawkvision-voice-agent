c4b95a8aa30cc56e3cfd2211aa066b1f5c5a3a72
hawkvisionprouk-spec <hawkvisionpro.uk@gmail.com>
2026-06-28 01:02:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ wss.on('connection', (twilioWs) => {
   const openAiConnect = () => {
     console.log('Connecting to OpenAI...');
     openAiWs = new WebSocket(
-      'wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview',
+      'wss://api.openai.com/v1/realtime?model=gpt-4o-mini-realtime-preview',
       {
         headers: {
           Authorization: `Bearer ${OPENAI_API_KEY}`,
